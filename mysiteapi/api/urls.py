2611a5789bcb86172e05d527f83706017bc442fe
yabsiraditya <yabsir.aditya@gmail.com>
2025-03-09 14:44:54 +0700

    testing api

--- mysiteapi/api/urls.py
+++ mysiteapi/api/urls.py
@@ -5,7 +5,7 @@ from rest_framework.routers import DefaultRouter
 urlpatterns = [
     path('products/', views.ProductListCreateApiView.as_view()),
     path('products/info/', views.ProductInfoAPIView.as_view()),
-    path('products/<int:product_id>/', views.ProductDetailApiView.as_view()),
+    path('products/<int:product_id>/', views.ProductDetailApiView.as_view(), name='product-detail'),
     path('users/', views.UserListView.as_view()),
 ]
 
